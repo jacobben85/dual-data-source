f99abcb9ab1ab00c91a961953995390ff3ba4f77
jacobben85 <jacobben85@gmail.com>
2016-03-22 21:42:24 -0400

    First commit

---
+++ src/main/java/com/jbjohn/spring/DualDataSourceApplication.java
@@ -0,0 +1,12 @@
+package com.jbjohn.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DualDataSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DualDataSourceApplication.class, args);
+	}
+}
